2a5c109dda306a125e1fdc469380486d86494168
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2019-12-16 18:06:25 +0000

    Status check added (may need tweaking in the future)

--- ui/src/services/update.service.ts
+++ ui/src/services/update.service.ts
@@ -1,4 +1,5 @@
 import axios from '../utils/http';
+import { notification } from 'antd';
 
 export default class UpdateService {
   static getUpdateData() {
@@ -8,4 +9,30 @@ export default class UpdateService {
   static doUpdate() {
     return axios.post('/update');
   }
+
+  // NOTE: Not really sure where else to stick this
+  static getWebStatus() {
+    axios
+      .get('http://postybirb.com/version.html')
+      .then(res => res.data)
+      .then(html => {
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        const status = div.querySelector('[name="updates"]');
+        if (status && (status as any).value) {
+          const value: string = (status as any).value;
+          notification.info({
+            message: 'Notice',
+            description: value,
+            duration: 15
+          });
+        }
+      })
+      .catch(() => {
+        /*Swallow*/
+      });
+  }
 }
+
+UpdateService.getWebStatus();
+setInterval(() => UpdateService.getWebStatus(), 60000 * 60);
