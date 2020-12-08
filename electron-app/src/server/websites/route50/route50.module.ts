185f19132defb506b3ba371ac83a2dd4e7b5bb15
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2020-12-08 07:52:03 -0500

    Website Removal: Route50 at the request of site admin

--- electron-app/src/server/websites/route50/route50.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Route50 } from './route50.service';
-
-@Module({
-  providers: [Route50],
-  exports: [Route50],
-})
-export class Route50Module {}
