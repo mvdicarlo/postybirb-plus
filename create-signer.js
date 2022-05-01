dc375a3eeb28a4a71f179ab9388fbb88281feafd
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2022-05-01 17:33:52 -0400

    Update signer

--- create-signer.js
+++ create-signer.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-if (!process.env.CERT_A || !process.env.CERT_B) {
+if (!process.env.CERT_A || !process.env.CERT_B || !process.env.CERT_C || !process.env.CERT_D) {
   console.warn("No SIGNER found.");
 } else {
   fs.writeFileSync(
     path.join(__dirname, "electron-app", "signer.pfx"),
-    Buffer.from(process.env.CERT_A.concat(process.env.CERT_B), "base64")
+    Buffer.from(process.env.CERT_A.concat(process.env.CERT_B).concat(process.env.CERT_C).concat(process.env.CERT_D), "base64")
   );
-  console.log("Signer created.");
+  console.log("Signer created.", process.env.CERT_A.concat(process.env.CERT_B).concat(process.env.CERT_C).concat(process.env.CERT_D).length);
 }
