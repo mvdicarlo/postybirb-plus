28feb639a2c8fa21c7200778f5eab79a39b2b103
Andy Neillans <andy@neillans.co.uk>
2023-04-02 16:42:23 +0100

    Mastodon tags support
    * Adding shx to support cross-platform mv
    * Adding tags onto Mastodon submissions
    * Limit tags to only be posted on public posts

--- electron-app/src/server/websites/mastodon/mastodon.service.ts
+++ electron-app/src/server/websites/mastodon/mastodon.service.ts
@@ -220,6 +220,19 @@ export class Mastodon extends Website {
         };
       }
 
+      // Tags only should be posted on public entries - they can not be searched on other types
+      if (options.visibility == 'public') {
+        // Update the post content with the Tags if any are specified - for Mastodon, we need to append 
+        // these onto the post, *IF* there is character count available.
+        data.tags.forEach(tag => {
+          let remain = maxChars - form.status.length;
+          if (remain > (tag.length)) {
+            form.status += ` ${tag}`
+          }
+          // We don't exit the loop, so we can cram in every possible tag, even if there are short ones!
+        })
+      }
+      
       if (options.spoilerText) {
         form.spoiler_text = options.spoilerText;
       }
