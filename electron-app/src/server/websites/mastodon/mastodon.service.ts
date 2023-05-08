fed63c121a8cf61d64616489fdfe0ba93bc1aa4c
Andy Neillans <andy@neillans.co.uk>
2023-05-08 22:22:15 +0100

    Change mastodon lib, start

--- electron-app/src/server/websites/mastodon/mastodon.service.ts
+++ electron-app/src/server/websites/mastodon/mastodon.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as MastodonInstance from 'mastodon-api';
+import generator, { Entity, Response } from 'megalodon'
 import {
   DefaultOptions,
   FileRecord,
@@ -30,7 +30,7 @@ import FormContent from 'src/server/utils/form-content.util';
 import WebsiteValidator from 'src/server/utils/website-validator.util';
 import { LoginResponse } from '../interfaces/login-response.interface';
 import { Website } from '../website.base';
-import * as _ from 'lodash';
+import _ from 'lodash';
 import WaitUtil from 'src/server/utils/wait.util';
 import { FileManagerService } from 'src/server/file-manager/file-manager.service';
 
@@ -55,8 +55,7 @@ export class Mastodon extends Website {
   constructor(private readonly fileRepository: FileManagerService) {
     super();
   }
-
-  readonly BASE_URL = '';
+  readonly BASE_URL: string;
   readonly enableAdvertisement = false;
   readonly acceptsAdditionalFiles = true;
   readonly defaultDescriptionParser = PlaintextParser.parse;
@@ -88,14 +87,10 @@ export class Mastodon extends Website {
     return status;
   }
 
-  private async getInstanceInfo(profileId: string, data: MastodonAccountData) {
-    const M = new MastodonInstance({
-      access_token: data.token,
-      api_url: `${data.website}/api/v2/`,
-    });
-
-    const instance = await M.get('instance');
-
+  private async getInstanceInfo(profileId: string, data: MastodonAccountData) {   
+    const client = generator('mastodon', data.website, data.token);
+    const instance = await client.getInstance();
+   
     this.storeAccountInformation(profileId, INFO_KEY, instance.data);
   }
 
@@ -105,11 +100,12 @@ export class Mastodon extends Website {
     return true;
   }
 
-  private getMastodonInstance(data: MastodonAccountData) {
-    return new MastodonInstance({
-      access_token: data.token,
-      api_url: `${data.website}/api/v1/`,
+  private getMastodonInstance(data: MastodonAccountData) : Entity.Instance {
+    const client = generator('mastodon', data.website, data.token);
+    client.getInstance().then((res) => {
+      return res.data;
     });
+    return null;
   }
 
   getScalingOptions(file: FileRecord, accountId: string): ScalingOptions {
@@ -123,11 +119,11 @@ export class Mastodon extends Website {
               ? instanceInfo.configuration.media_attachments.image_size_limit
               : instanceInfo.configuration.media_attachments.video_size_limit,
         }
-      : {
+      : {           
           maxHeight: 4000,
           maxWidth: 4000,
-          maxSize: FileSize.MBtoBytes(300),
-        };
+          maxSize: FileSize.MBtoBytes(300) 
+      };
   }
 
   private async uploadMedia(
@@ -192,7 +188,7 @@ export class Mastodon extends Website {
     data: FilePostData<MastodonFileOptions>,
     accountData: MastodonAccountData,
   ): Promise<PostResponse> {
-    const M = this.getMastodonInstance(accountData);
+    const M = generator('mastodon', accountData.website, accountData.token);
 
     const files = [data.primary, ...data.additional];
     this.checkCancelled(cancellationToken);
@@ -204,66 +200,82 @@ export class Mastodon extends Website {
     }
 
     const instanceInfo: MastodonInstanceInfo = this.getAccountInfo(data.part.accountId, INFO_KEY);
-    const chunkCount = instanceInfo
-      ? instanceInfo?.configuration?.statuses?.max_media_attachments
-      : 4;
+    const chunkCount = instanceInfo ? instanceInfo?.configuration?.statuses?.max_media_attachments : 4;
     const maxChars = instanceInfo ? instanceInfo?.configuration?.statuses?.max_characters : 500;
 
     const isSensitive = data.rating !== SubmissionRating.GENERAL;
     const { options } = data;
     const chunks = _.chunk(uploadedMedias, chunkCount);
     let lastId = undefined;
+
     for (let i = 0; i < chunks.length; i++) {
+      let statusOptions: any = {
+        status: '',
+        sensitive: isSensitive,
+        visibility: options.visibility || 'public',
+        spoiler_text: ''
+      };
+      let status = undefined;
+
       let form = undefined;
       if (i === 0) {
-        form = {
-          status: `${options.useTitle && data.title ? `${data.title}\n` : ''}${
-            data.description
-          }`.substring(0, maxChars),
+        status = `${options.useTitle && data.title ? `${data.title}\n` : ''}${
+          data.description
+        }`.substring(0, maxChars)
+
+        statusOptions = {
           sensitive: isSensitive,
           visibility: options.visibility || 'public',
-          media_ids: chunks[i].map((media) => media.id),
-        };
-      } else {
-        form = {
-          sensitive: isSensitive,
-          visibility: options.visibility || 'public',
-          media_ids: chunks[i].map((media) => media.id),
-          in_reply_to_id: lastId,
-        };
-      }
-
-      const tags = this.formatTags(data.tags);
-
-      // Update the post content with the Tags if any are specified - for Mastodon, we need to append
-      // these onto the post, *IF* there is character count available.
-      if (tags.length > 0) {
-        form.status += '\n\n';
-      }
-
-      tags.forEach((tag) => {
-        let remain = maxChars - form.status.length;
-        let tagToInsert = tag;
-        if (remain > tagToInsert.length) {
-          form.status += ` ${tagToInsert}`;
+          media_ids: chunks[i].map((media) => media.id), 
+          spoiler_text: "",
         }
-        // We don't exit the loop, so we can cram in every possible tag, even if there are short ones!
+     } else {
+      statusOptions = {
+        sensitive: isSensitive,
+        visibility: options.visibility || 'public',
+        media_ids: chunks[i].map((media) => media.id),
+        in_reply_to_id: lastId,  
+        spoiler_text: "",    
+      }      
+     }
+
+      const tags: string[] = this.parseTags(data.tags, {
+        spaceReplacer: '_',
+        minLength: 1,
+        maxLength: 100,
       });
 
+      // Update the post content with the Tags if any are specified - for Mastodon, we need to append 
+      // these onto the post, *IF* there is character count available.
+      if (tags.length > 0) {
+        status += "\n\n";
+      }
+
+      tags.forEach(tag => {
+        let remain = maxChars - form.status.length;
+        let tagToInsert = tag;
+        if (!tag.startsWith('#')) {
+          tagToInsert = `#${tagToInsert}`
+        }
+        if (remain > (tagToInsert.length)) {
+          status += ` ${tagToInsert}`
+        }
+        // We don't exit the loop, so we can cram in every possible tag, even if there are short ones!
+      })
+      
       if (options.spoilerText) {
-        form.spoiler_text = options.spoilerText;
+        statusOptions.spoiler_text = options.spoilerText;
       }
 
-      const post = await M.post('statuses', form);
-      lastId = post.data.id;
-
-      if (!lastId || post.data.error) {
+      M.postStatus(status, statusOptions).then((result) => {
+        lastId = result.data.id;
+        let res = result.data as Entity.Status;
+        return this.createPostResponse({ source: res.url });
+      }).catch((err: Error) => {
         return Promise.reject(
-          this.createPostResponse({ message: post.data.error, additionalInfo: post.data }),
+          this.createPostResponse({ message: err.message }),
         );
-      } else {
-        return this.createPostResponse({ source: post.data.url });
-      }
+      })
     }
 
     this.checkCancelled(cancellationToken);
@@ -276,64 +288,59 @@ export class Mastodon extends Website {
     data: PostData<Submission, MastodonNotificationOptions>,
     accountData: MastodonAccountData,
   ): Promise<PostResponse> {
-    const M = this.getMastodonInstance(accountData);
+    const M = generator('mastodon', accountData.website, accountData.token);
     const instanceInfo: MastodonInstanceInfo = this.getAccountInfo(data.part.accountId, INFO_KEY);
     const maxChars = instanceInfo ? instanceInfo?.configuration?.statuses?.max_characters : 500;
 
     const isSensitive = data.rating !== SubmissionRating.GENERAL;
 
     const { options } = data;
-    const form: any = {
-      status: `${options.useTitle && data.title ? `${data.title}\n` : ''}${data.description}`,
+    let status = `${options.useTitle && data.title ? `${data.title}\n` : ''}${data.description}`;
+    const statusOptions: any = {
       sensitive: isSensitive,
       visibility: options.visibility || 'public',
+      spoiler_text: ""
     };
 
-    const tags = this.formatTags(data.tags);
-
-    // Update the post content with the Tags if any are specified - for Mastodon, we need to append
-    // these onto the post, *IF* there is character count available.
-    if (tags.length > 0) {
-      form.status += '\n\n';
-    }
-
-    tags.forEach((tag) => {
-      let remain = maxChars - form.status.length;
-      let tagToInsert = tag;
-      if (remain > tagToInsert.length) {
-        form.status += ` ${tagToInsert}`;
-      }
-      // We don't exit the loop, so we can cram in every possible tag, even if there are short ones!
+    const tags: string[] = this.parseTags(data.tags, {
+      spaceReplacer: '_',
+      minLength: 1,
+      maxLength: 100,
     });
 
+    // Update the post content with the Tags if any are specified - for Mastodon, we need to append 
+    // these onto the post, *IF* there is character count available.
+    if (tags.length > 0) {
+      status += "\n\n";
+    }
+
+    tags.forEach(tag => {
+      let remain = maxChars - status.length;
+      let tagToInsert = tag;
+      if (!tag.startsWith('#')) {
+        tagToInsert = `#${tagToInsert}`
+      }
+      if (remain > (tagToInsert.length)) {
+        status += ` ${tagToInsert}`
+      }
+      // We don't exit the loop, so we can cram in every possible tag, even if there are short ones!
+    })
+
     if (options.spoilerText) {
-      form.spoiler_text = options.spoilerText;
+      statusOptions.spoiler_text = options.spoilerText;
     }
 
     this.checkCancelled(cancellationToken);
-    const post = await M.post('statuses', form);
-
-    if (!post.data.id || post.data.error) {
+    
+    M.postStatus(status, statusOptions).then((result) => {
+      let res = result.data as Entity.Status;
+      return this.createPostResponse({ source: res.url });
+    }).catch((err: Error) => {
       return Promise.reject(
-        this.createPostResponse({ message: post.data.error, additionalInfo: post.data }),
+        this.createPostResponse({ message: err.message }),
       );
-    }
-
-    return this.createPostResponse({ source: post.data.url });
-  }
-
-  formatTags(tags: string[]) {
-    return this.parseTags(
-      tags
-        .map((tag) => tag.replace(/[^a-z0-9]/gi, ' '))
-        .map((tag) =>
-          tag
-            .split(' ')
-            // .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(''),
-        ),
-      { spaceReplacer: '' },
-    ).map((tag) => `#${tag}`);
+    })
+    return this.createPostResponse({});
   }
 
   validateFileSubmission(
@@ -392,23 +399,20 @@ export class Mastodon extends Website {
 
       // Check the image dimensions are not over 4000 x 4000 - this is the Mastodon server max
       if (
-        isAutoscaling &&
-        type === FileSubmissionType.IMAGE &&
-        (file.height > 4000 || file.width > 4000)
-      ) {
-        warnings.push(
-          `${name} will be scaled down to a maximum size of 4000x4000, while maintaining aspect ratio`,
-        );
-      }
+        isAutoscaling && 
+        type === FileSubmissionType.IMAGE && 
+        (file.height > 4000 || file.width > 4000)) {
+          warnings.push(`${name} will be scaled down to a maximum size of 4000x4000, while maintaining
+           aspect ratio`);
+        }
     });
 
-    if (
-      (submissionPart.data.tags.value.length > 1 || defaultPart.data.tags.value.length > 1) &&
-      submissionPart.data.visibility != 'public'
-    ) {
-      warnings.push(
-        `This post won't be listed under any hashtag as it is not public. Only public posts can be searched by hashtag.`,
-      );
+    if ((submissionPart.data.tags.value.length > 1 || defaultPart.data.tags.value.length > 1) && 
+      submissionPart.data.visibility != "public") {
+        warnings.push(
+              `This post won't be listed under any hashtag as it is not public. Only public posts 
+              can be searched by hashtag.`,
+            );
     }
 
     return { problems, warnings };
