18a58224d14edbc6ba085162dac52f433ef3e661
Andy Neillans <andy@neillans.co.uk>
2023-06-17 17:50:19 +0100

    WIP: Misskey / Calckey

--- electron-app/src/server/websites/misskey/misskey.module.ts
+++ electron-app/src/server/websites/misskey/misskey.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MissKey } from './misskey.service';
 import { FileManagerModule } from 'src/server/file-manager/file-manager.module';
+import { MissKeyController } from './misskey.controller';
 
 @Module({
+  controllers: [MissKeyController],
   providers: [MissKey],
   exports: [MissKey],
   imports: [FileManagerModule],
