e78f3a09284163a327334300b815e56797bc6b60
Alex Losego <shinx@isofrieze.com>
2023-08-16 10:05:24 -0400

    Fix Itaku posting
    "true"

--- electron-app/src/server/websites/itaku/itaku.service.ts
+++ electron-app/src/server/websites/itaku/itaku.service.ts
@@ -140,7 +140,7 @@ export class Itaku extends Website {
     };
     
     if (data.options.shareOnFeed) {
-      postData.add_to_feed = true;
+      postData.add_to_feed = "true";
     }
 
     if (data.options.spoilerText) {
