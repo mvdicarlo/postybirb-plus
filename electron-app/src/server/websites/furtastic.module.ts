597d3604ae3ecb17468c5f670377b174c3cd2e0f
Xenoyia <xenthekobold@gmail.com>
2023-09-03 20:05:07 +0100

    Delete electron-app/src/server/websites/furtastic.module.ts

--- electron-app/src/server/websites/furtastic.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Furtastic } from './furtastic.service';
-
-@Module({
-  providers: [Furtastic],
-  exports: [Furtastic],
-})
-export class FurtasticModule {}
