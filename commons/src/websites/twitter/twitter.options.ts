dd1da63875ae4311525522894daa1a0f8d0ea72f
Andy Neillans <andy@neillans.co.uk>
2023-10-11 20:43:58 +0100

    Removes Twitter and Furry-Life (defunct)

--- commons/src/websites/twitter/twitter.options.ts
+++
@@ -1,7 +0,0 @@
-import { DefaultOptionsEntity } from '../../models/default-options.entity';
-import { TwitterFileOptionsEntity } from './twitter.file.options';
-
-export class Twitter {
-  static readonly FileOptions = TwitterFileOptionsEntity;
-  static readonly NotificationOptions = DefaultOptionsEntity;
-}
