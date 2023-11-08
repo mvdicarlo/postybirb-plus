ac49ccfffcdbdc9496a6ee7a04ee73cc435f4259
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2023-11-08 17:47:36 +0300

    Fix discord link and add bluesky login warnings

--- ui/src/websites/bluesky/BlueskyLogin.tsx
+++ ui/src/websites/bluesky/BlueskyLogin.tsx
@@ -1,6 +1,5 @@
-import { Button, Form, Input, message, Spin } from 'antd';
+import { Alert, Button, Form, Input, message } from 'antd';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BlueskyAccountData } from 'postybirb-commons';
 import LoginService from '../../services/login.service';
 import { LoginDialogProps } from '../interfaces/website.interface';
@@ -14,39 +13,34 @@ export default class BlueskyLogin extends React.Component<LoginDialogProps, Stat
   state: State = {
     username: '',
     password: '',
-    loading: true
+    loading: true,
   };
-
-  private view: any;
 
   constructor(props: LoginDialogProps) {
     super(props);
     this.state = {
       ...this.state,
-      ...(props.data as State)
+      ...(props.data as State),
     };
   }
 
-
-
-
   submit() {
-    const agent = new BskyAgent({ service: 'https://bsky.social' })
-    
-    agent.login({
-      identifier: this.state.username,
-      password: this.state.password,
-    }).then(res => {
-      if (res.success) { 
-        LoginService.setAccountData(this.props.account._id, this.state).then(
-            () => {
-              message.success(`Details confirmed and saved`);
-            }
-          );
-      } else {
-        message.error(`Failed to authenticate.`);
-      }
-    });
+    const agent = new BskyAgent({ service: 'https://bsky.social' });
+
+    agent
+      .login({
+        identifier: this.state.username,
+        password: this.state.password,
+      })
+      .then(res => {
+        if (res.success) {
+          LoginService.setAccountData(this.props.account._id, this.state).then(() => {
+            message.success(`Details confirmed and saved`);
+          });
+        } else {
+          message.error(`Failed to authenticate.`);
+        }
+      });
   }
 
   isValid(): boolean {
@@ -58,21 +52,52 @@ export default class BlueskyLogin extends React.Component<LoginDialogProps, Stat
       <div className="h-full">
         <div className="container">
           <Form layout="vertical">
-            <Form.Item label="Username" help="Your DID or handle - for example yourname.bsky.social" required>
+            <Form.Item
+              label="Username"
+              help="Your DID or handle - for example yourname.bsky.social"
+              required
+            >
               <Input
                 value={this.state.username}
                 onChange={({ target }) => this.setState({ username: target.value })}
               />
+              {this.state.username.startsWith('@') && (
+                <Alert
+                  type="warning"
+                  message="You don't need to input the @"
+                  description={
+                    <div>
+                      Unless your username <strong>really</strong> contains it
+                    </div>
+                  }
+                />
+              )}
+              {this.state.username && !this.state.username.includes('.') && (
+                <Alert
+                  type="warning"
+                  message="Be sure that the username is in the format handle.bsky.social"
+                  description={
+                    <div>
+                      Or if you are using custom domain, make sure to include full username, e.g.
+                      domain.ext or
+                    </div>
+                  }
+                />
+              )}
             </Form.Item>
-            <Form.Item label="Password" help="An *app* password - you can get one of these in Settings" required>
+            <Form.Item
+              label="Password"
+              help="An *app* password - you can get one of these in Settings"
+              required
+            >
               <Input
                 value={this.state.password}
                 onChange={({ target }) => this.setState({ password: target.value })}
-              />        
-            </Form.Item>                    
+              />
+            </Form.Item>
           </Form>
           <Button onClick={this.submit.bind(this)} disabled={!this.isValid()}>
-                    Save
+            Save
           </Button>
         </div>
       </div>
