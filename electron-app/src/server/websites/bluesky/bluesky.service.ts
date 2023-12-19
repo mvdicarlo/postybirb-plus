7ddb8f96bf01601bbb15a27e4d7ac67dde45f7bd
Andy Neillans <andy@neillans.co.uk>
2023-12-19 13:42:27 +0000

    Fix

--- electron-app/src/server/websites/bluesky/bluesky.service.ts
+++ electron-app/src/server/websites/bluesky/bluesky.service.ts
@@ -263,7 +263,7 @@ export class Bluesky extends Website {
       let friendlyUrl = `https://${server}/profile/${handle}/post/${postId}`;
 
       // After the post has been made, check to see if we need to set a ThreadGate; these are the options to control who can reply to your post, and need additional calls
-      if (data.options.threadgate != "") {
+      if (data.options.threadgate !== "") {
         this.createThreadgate(agent, postResult.uri, data.options.threadgate);
       }
 
