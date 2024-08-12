ab4e44d1135d501070a8e4c651423a6ec02ba3bb
Michael DiCarlo <michael.v.dicarlo@gmail.com>
2024-08-12 08:18:00 -0400

    fix: fixes maturity rating setting in deviant art

--- electron-app/src/server/websites/deviant-art/deviant-art.service.ts
+++ electron-app/src/server/websites/deviant-art/deviant-art.service.ts
@@ -161,9 +161,9 @@ export class DeviantArt extends Website {
     this.checkCancelled(cancellationToken);
     const mature =
       data.options.isMature ||
-      data.options.rating === SubmissionRating.ADULT ||
-      data.options.rating === SubmissionRating.MATURE ||
-      data.options.rating === SubmissionRating.EXTREME;
+      data.rating === SubmissionRating.ADULT ||
+      data.rating === SubmissionRating.MATURE ||
+      data.rating === SubmissionRating.EXTREME;
     const description = await BrowserWindowUtil.runScriptOnPage<string>(
       data.part.accountId,
       this.BASE_URL,
